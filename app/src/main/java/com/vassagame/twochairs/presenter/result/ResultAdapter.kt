1e51340f14b3e3cd9b9f950cf5bcdf6ba5e7b554
dev_m <doliv1092000@gmail.com>
2022-09-13 18:55:32 +0300

    Created layout for RecycleView ResultFragment add icons like and dislike

---
+++ app/src/main/java/com/vassagame/twochairs/presenter/result/ResultAdapter.kt
@@ -0,0 +1,4 @@
+package com.vassagame.twochairs.presenter.result
+
+class ResultAdapter {
+}
--- app/src/main/java/com/vassagame/twochairs/presenter/result/ResultFragment.kt
+++ app/src/main/java/com/vassagame/twochairs/presenter/result/ResultFragment.kt
@@ -6,10 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.vassagame.twochairs.R
+import com.vassagame.twochairs.databinding.FragmentOptionGameBinding
+import com.vassagame.twochairs.databinding.FragmentResultBinding
 
 
 class ResultFragment : Fragment() {
-
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +22,15 @@ class ResultFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+    ): View {
+
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView
 
-
+    }
 }
