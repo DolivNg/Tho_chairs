07f15fe45356f2a04a8a449027e68d1d82ca15c1
dev_m <doliv1092000@gmail.com>
2022-11-18 17:30:22 +0200

    Open db in DI

--- app/src/main/java/com/vassagame/twochairs/di/DataModule.kt
+++ app/src/main/java/com/vassagame/twochairs/di/DataModule.kt
@@ -1,11 +1,32 @@
 package com.vassagame.twochairs.di
 
+
+import android.app.Application
+import androidx.room.Room
 import com.vassagame.twochairs.data.GameRepositoryImpl
+import com.vassagame.twochairs.data.db.AppDatabase
 import com.vassagame.twochairs.domain.reposytory.GameRepository
+
 import org.koin.dsl.module
 
 val dataModule = module {
-    single<GameRepository>{
-        GameRepositoryImpl(context = get())
+
+    fun provideDatabase(application: Application): AppDatabase {
+        return Room.databaseBuilder(application, AppDatabase::class.java, "question.db")
+            .createFromAsset("database/questionDB.db")
+            .build()
     }
+
+//    fun provideCountriesDao(database: CountriesDatabase): CountriesDao {
+//        return  database.countriesDao
+//    }
+
+    single { provideDatabase(get()) }
+    //single { provideCountriesDao(get()) }
+
+
+    single<GameRepository>{
+        GameRepositoryImpl(context = get(), db = get())
+    }
+
 }
