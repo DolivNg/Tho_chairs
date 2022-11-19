a88720d4859e0f7d4fe996bb1a78611602d7ce76
dev_m <doliv1092000@gmail.com>
2022-11-19 12:14:16 +0200

    change name Repository interface and implementation to better name

---
+++ app/src/main/java/com/vassagame/twochairs/domain/reposytory/QuestionRepository.kt
@@ -0,0 +1,23 @@
+package com.vassagame.twochairs.domain.reposytory
+
+import com.vassagame.twochairs.domain.entity.dbentity.PackDomEntity
+import com.vassagame.twochairs.domain.entity.dbentity.QuestionDomEntity
+
+interface QuestionRepository {
+
+
+    suspend fun getPackAll() : ArrayList<PackDomEntity>
+
+    suspend fun addNewPack(pack : PackDomEntity)
+
+    suspend fun removePack(packName : String)
+
+
+    suspend fun getQuestionAll() : ArrayList<QuestionDomEntity>
+
+    suspend fun addNewQuestion(pack : PackDomEntity)
+
+    suspend fun removeQuestion(id : Int)
+
+
+}
