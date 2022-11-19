8d97975a0aa82037dda3da7ed0a2dfe5213950cb
dev_m <doliv1092000@gmail.com>
2022-11-19 12:33:13 +0200

    change to static

--- app/src/main/java/com/vassagame/twochairs/data/db/Mapper/PackMapper.kt
+++ app/src/main/java/com/vassagame/twochairs/data/db/Mapper/PackMapper.kt
@@ -4,43 +4,45 @@ import com.vassagame.twochairs.data.db.entity.PackEntity
 import com.vassagame.twochairs.domain.entity.dbentity.PackDomEntity
 
  class PackMapper {
+     companion object {
+         fun packDataToDomain(packData: PackEntity): PackDomEntity {
+             return PackDomEntity(
+                 id = packData.id,
+                 packName = packData.packName,
+                 standard = packData.standard,
+                 selected = packData.selected
+             )
+         }
 
-    fun packDataToDomain(packData: PackEntity): PackDomEntity {
-        return PackDomEntity(
-            id = packData.id,
-            packName = packData.packName,
-            standard = packData.standard,
-            selected = packData.selected
-        )
-    }
+         fun packArrayDataToDomain(packArrayData: ArrayList<PackEntity>): ArrayList<PackDomEntity> {
+             val array = ArrayList<PackDomEntity>()
+             packArrayData.forEach {
+                 array.add(
+                     packDataToDomain(it)
+                 )
+             }
+             return array
+         }
 
-    fun packArrayDataToDomain(packArrayData: ArrayList<PackEntity>): ArrayList<PackDomEntity> {
-        val array = ArrayList<PackDomEntity>()
-        packArrayData.forEach {
-            array.add(
-                packDataToDomain(it)
-            )
-        }
-        return array
-    }
+         fun packDomainToData(packDomain: PackDomEntity): PackEntity {
+             return PackEntity(
+                 id = packDomain.id,
+                 packName = packDomain.packName,
+                 standard = packDomain.standard,
+                 selected = packDomain.selected
+             )
+         }
 
-    fun packDomainToData(packDomain: PackDomEntity): PackEntity {
-        return PackEntity(
-            id = packDomain.id,
-            packName = packDomain.packName,
-            standard = packDomain.standard,
-            selected = packDomain.selected
-        )
-    }
+         fun packArrayDomainToData(packArrayDomain: ArrayList<PackDomEntity>): ArrayList<PackEntity> {
+             val array = ArrayList<PackEntity>()
+             packArrayDomain.forEach {
+                 array.add(
+                     packDomainToData(it)
+                 )
+             }
+             return array
+         }
+     }
 
-    fun packArrayDomainToData(packArrayDomain: ArrayList<PackDomEntity>): ArrayList<PackEntity> {
-        val array = ArrayList<PackEntity>()
-        packArrayDomain.forEach {
-            array.add(
-                packDomainToData(it)
-            )
-        }
-        return array
-    }
 
 }
